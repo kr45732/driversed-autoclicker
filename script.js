29d3b27a815ae82c6854d3b1ea97ca67e47ce3fa
kr45732 <52721908+kr45732@users.noreply.github.com>
2022-06-12 13:55:23 -0400

    Focus when on dashboard to prevent security questions from locking you out

--- script.js
+++ script.js
@@ -5,11 +5,15 @@
 // @description  Auto-clicks the "next" button (pauses when on questions) on the driversed.com course
 // @author       kr45732
 // @match        https://driversed.com/*
-// @grant        none
+// @grant        window.focus
 // ==/UserScript==
 
 setInterval(() => {
     if (document.getElementsByClassName('result-headertxt')[0]?.textContent?.trim() != "Questions") {
         Array.prototype.filter.call(document.getElementsByClassName('btn'), button => button?.textContent?.trim() == "NEXT")[0]?.click()
+    }
+
+    if (location.pathname.endsWith("/dashboard/") && !document.hasFocus()) {
+        window.focus();
     }
 }, 400)
